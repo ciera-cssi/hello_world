59e4862bb9629dbcdefb08a05fecc04cb4344269
Ciera Jaspan <cssi@cssis-MacBook-Pro.local>
2019-07-22 12:05:19 -0700

    Now using Jinja to serve the HTML

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import webapp2
 import logging
+# Step 1: Import jinja and os
+import jinja2
+import os
+
+# Step 2: Set up Jinja Environment
+jinja_env = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(os.path.dirname(__file__))
+)
+
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         logging.info('In hello handler')
-        self.response.write('<h2>Hello CSSI!</h2>')
-        self.response.write('<i>I hope you are having a fun week!</i>')
-        self.response.write('<ul>')
-        self.response.write('<li>Robot controller')
-        self.response.write('<li>pterodactyl')
-        self.response.write('<li>Evolution')
-        self.response.write('</ul>')
-        
+        # Step 3: use the jinja environment to get our html
+        template = jinja_env.get_template('hello.html')
+        self.response.write(template.render())
+
+
 class SecretPage(webapp2.RequestHandler):
     def get(self):
         self.response.write('Shhh! This is the secret entrance!')
