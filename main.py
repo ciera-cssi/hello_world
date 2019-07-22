6c0b051b0832a44a1ec4041b23a444e331b3a4a6
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-22 11:42:58 -0700

    Added some html and welcome message to my hello handler

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ import logging
 class MainPage(webapp2.RequestHandler):
     def get(self):
         logging.info('In hello handler')
-        self.response.write('Hello World!')
-
+        self.response.write('<h2>Hello CSSI!</h2>')
+        self.response.write('<i>I hope you are having a fun week!</i>')
+        self.response.write('<ul>')
+        self.response.write('<li>Robot controller')
+        self.response.write('<li>pterodactyl')
+        self.response.write('<li>Evolution')
+        self.response.write('</ul>')
+        
 class SecretPage(webapp2.RequestHandler):
     def get(self):
         self.response.write('Shhh! This is the secret entrance!')
