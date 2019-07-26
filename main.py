c71067212b6a2b1135626cd178dbc99124c53ae3
Ciera Jaspan <cssi@cssis-MacBook-Pro.local>
2019-07-26 09:58:00 -0700

    something

--- main.py
+++ main.py
@@ -14,9 +14,31 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         logging.info('In hello handler')
         # Step 3: use the jinja environment to get our html
-        template = jinja_env.get_template('hello.html')
+        template = jinja_env.get_template('templates/hello.html')
         self.response.write(template.render())
 
+class AllStudentsPage(webapp2.RequestHandler):
+    def get(self):
+        cssi = [
+            {"name":"Asia", "university": "SDSU"},
+            {"name":"Taylore", "university": "Stanford"},
+            {"name":"Brian", "university": "UT%20Austin"},
+            {"name":"Zach", "university": "UC%20Irvine"},
+        ]
+        template_vars = {
+            "cssi" : cssi,
+        }
+        template = jinja_env.get_template('templates/all_students.html')
+        self.response.write(template.render(template_vars))
+
+class StudentPage(webapp2.RequestHandler):
+    def get(self):
+        template_vars = {
+            'name': self.request.get('student_name'),
+            'university': self.request.get('university')
+        }
+        template = jinja_env.get_template('templates/student.html')
+        self.response.write(template.render(template_vars))
 
 class SecretPage(webapp2.RequestHandler):
     def get(self):
@@ -28,6 +50,8 @@ class AboutPage(webapp2.RequestHandler):
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/all_students', AllStudentsPage),
+    ('/student', StudentPage),
     ('/secretentrance', SecretPage),
     ('/about', AboutPage),
 ], debug=True)
